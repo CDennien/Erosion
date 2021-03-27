c3da56c55cc82c8978a49052d356f768cec6c8d5
Callum Dennien <callum.callumdennien@my.jcu.edu.au>
2021-03-27 12:40:19 +1000

    Add TrayIcon for program.

--- src/com/callumdennien/Erosion.java
+++ src/com/callumdennien/Erosion.java
@@ -6,11 +6,16 @@ import com.sun.jna.win32.StdCallLibrary;
 import com.sun.jna.win32.W32APIFunctionMapper;
 import com.sun.jna.win32.W32APITypeMapper;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.HashMap;
 
 public class Erosion {
     private static final SoupBowl soupBowl = new SoupBowl();
-    private static final String path = "C:\\Users\\" + System.getProperty("user.name") + "\\Documents\\erosion_background.jpg";
+    private static final String path = "C:\\Users\\" + System.getProperty("user.name") + "\\Documents\\Erosion\\erosion_background.jpg";
 
     public static void main(String[] args) throws Throwable {
         String postURL = soupBowl.getSoupTopping();
@@ -18,6 +23,39 @@ public class Erosion {
 
         SPI.INSTANCE.SystemParametersInfo(new UINT_PTR(SPI.SPI_SETDESKWALLPAPER), new UINT_PTR(0), path, new UINT_PTR(SPI.SPIF_UPDATEINIFILE | SPI.SPIF_SENDWININICHANGE));
 
+        final TrayIcon trayIcon;
+
+        if (SystemTray.isSupported()) {
+            SystemTray tray = SystemTray.getSystemTray();
+            Image image = Toolkit.getDefaultToolkit().getImage("C:\\Users\\" + System.getProperty("user.name") + "\\Documents\\Erosion\\logo.png");
+
+            ActionListener exitListener = e -> {
+                System.exit(0);
+            };
+
+            ActionListener changeListener = e -> {
+                System.exit(0);
+            };
+
+            MenuItem defaultItem = new MenuItem("Exit");
+            defaultItem.addActionListener(exitListener);
+            MenuItem change = new MenuItem("Change");
+            change.addActionListener(changeListener);
+
+            PopupMenu popup = new PopupMenu();
+            popup.add(change);
+            popup.add(defaultItem);
+
+            try {
+                trayIcon = new TrayIcon(image, "Erosion", popup);
+                trayIcon.setImageAutoSize(true);
+                tray.add(trayIcon);
+
+            } catch (AWTException e) {
+                System.err.println("TrayIcon could not be added.");
+            }
+
+        }
     }
 
     public interface SPI extends StdCallLibrary {
@@ -25,7 +63,7 @@ public class Erosion {
         long SPIF_UPDATEINIFILE = 0x01;
         long SPIF_SENDWININICHANGE = 0x02;
 
-        SPI INSTANCE = (SPI) Native.load("user32", SPI.class, new HashMap<String, Object>() {
+        SPI INSTANCE = Native.load("user32", SPI.class, new HashMap<>() {
             {
                 put(OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
                 put(OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
