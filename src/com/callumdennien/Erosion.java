66d7986d8b446a831e631d0179466215a56377a2
Callum Dennien <callum.callumdennien@my.jcu.edu.au>
2021-03-27 11:54:05 +1000

    Make background change using JNA and JNA platform.

--- src/com/callumdennien/Erosion.java
+++ src/com/callumdennien/Erosion.java
@@ -1,12 +1,40 @@
 package com.callumdennien;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.WinDef.UINT_PTR;
+import com.sun.jna.win32.*;
+
+import java.util.HashMap;
 
 public class Erosion {
     private static final SoupBowl soupBowl = new SoupBowl();
+    private static final String path = "C:\\Users\\" + System.getProperty("user.name") + "\\Documents\\erosion_background.jpg";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Throwable {
         String postURL = soupBowl.getSoupTopping();
+        soupBowl.spillSoup(postURL, path);
+
+        SPI.INSTANCE.SystemParametersInfo(new UINT_PTR(SPI.SPI_SETDESKWALLPAPER), new UINT_PTR(0), path, new UINT_PTR(SPI.SPIF_UPDATEINIFILE | SPI.SPIF_SENDWININICHANGE));
+
+    }
+
+    public interface SPI extends StdCallLibrary {
+        long SPI_SETDESKWALLPAPER = 20;
+        long SPIF_UPDATEINIFILE = 0x01;
+        long SPIF_SENDWININICHANGE = 0x02;
+
+        SPI INSTANCE = (SPI) Native.load("user32", SPI.class, new HashMap<String, Object>() {
+            {
+                put(OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
+                put(OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
+            }
+        });
+
+        void SystemParametersInfo(
+                UINT_PTR uiAction,
+                UINT_PTR uiParam,
+                String pvParam,
+                UINT_PTR fWinIni
+        );
     }
 }
--- src/com/callumdennien/SoupBowl.java
+++ src/com/callumdennien/SoupBowl.java
@@ -1,15 +1,20 @@
 package com.callumdennien;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class SoupBowl {
-    private static final String url = "https://www.reddit.com/r/wallpapers/hot/";
+    private static final String url = "https://www.reddit.com/r/wallpapers/top/";
 
     public String getSoupTopping() throws IOException {
         Document doc = Jsoup.connect(url).get();
@@ -21,13 +26,27 @@ public class SoupBowl {
             posts.add(post);
         }
 
-        String postURL = getSoupImage(posts);
+        return getSoupImage(posts);
+    }
 
-        return postURL;
+    public void spillSoup(String postURL, String path) throws IOException {
+        URL url = new URL(postURL);
+        InputStream is = url.openStream();
+        OutputStream os = new FileOutputStream(path);
+
+        byte[] b = new byte[2048];
+        int length;
+
+        while ((length = is.read(b)) != -1) {
+            os.write(b, 0, length);
+        }
+
+        is.close();
+        os.close();
     }
 
     private String getSoupImage(ArrayList<String> posts) throws IOException {
-        int random = (int) (Math.random() * (posts.size() - 1 + 1) + 1);
+        int random = (int) (Math.random() * (posts.size() - 2 + 1) + 1);
 
         Document doc = Jsoup.connect(posts.get(random)).get();
         Elements elements = doc.getElementsByClass("may-blank");
